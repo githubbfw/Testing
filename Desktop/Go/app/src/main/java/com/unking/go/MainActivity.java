4dee5d60bbc6d6789111bc127a5d985964131416
bifengwu <778418575@qq.com>
2018-06-10 19:29:06 +0800

    第一次提交项目

---
+++ Desktop/Go/app/src/main/java/com/unking/go/MainActivity.java
@@ -0,0 +1,14 @@
+package com.unking.go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //提交新内容
+    }
+}
