1c592c0f2e395eb0301e8e82cc5ea561ca88d2c9
kyzgbfw <778418575@qq.com>
2018-06-10 16:29:11 +0800

    第610次提交项目

--- Desktop/firstdemo/app/src/main/java/com/unking/firstdemo/MainActivity.java
+++ Desktop/firstdemo/app/src/main/java/com/unking/firstdemo/MainActivity.java
@@ -10,17 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-
-
-
-
-
-
-
-
+       //添加了一行注释的代码
 
     }
 }
