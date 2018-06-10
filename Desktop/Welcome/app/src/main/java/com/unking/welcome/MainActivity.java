f3f83e5014c6758fe560200a20de1647375ec573
bifengwu <778418575@qq.com>
2018-06-10 22:29:43 +0800

    第一次提交项目

---
+++ Desktop/Welcome/app/src/main/java/com/unking/welcome/MainActivity.java
@@ -0,0 +1,14 @@
+package com.unking.welcome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //提交项目
+    }
+}
